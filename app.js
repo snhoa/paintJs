f6864469a1abe4ad70b48f4b1bf45eb85b1c14bf
DongJin Kim <your@email.com>
2021-01-20 15:04:55 +0900

    2d context

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,12 +10,22 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
 
-    console.log(x,y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
@@ -19,12 +33,9 @@ function onMouseDown(event){
     console.log(event);
 }
 
-function onMouseUp(event){
-}
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
